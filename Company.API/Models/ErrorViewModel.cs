8e4994a4e83b68fa13d6728397815e729d37c93d
Sameeh <sameeh3hamdan@gmail.com>
2022-07-25 15:33:35 +0300

    Add project files.

---
+++ Company.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Company.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
